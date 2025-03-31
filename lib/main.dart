5836ea59bf39ea60e70fd0e345dc3ccd51a27bad
nataliahrabovska <natagrabovskanata@gmail.com>
2025-03-31 17:17:07 +0300

    feat: started implementing second lab

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,190 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(GrainDocApp());
 
-class MyApp extends StatelessWidget {
+class GrainDocApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HedgehogCounter(),
+      title: 'GrainDoc',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        fontFamily: 'Montserrat',
+        scaffoldBackgroundColor: Color(0xFFFFFCF6),
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/register': (context) => RegisterPage(),
+        '/profile': (context) => ProfilePage(),
+        '/home': (context) => HomePage(),
+      },
     );
   }
 }
 
-class HedgehogCounter extends StatefulWidget {
-  @override
-  _HedgehogCounterState createState() => _HedgehogCounterState();
-}
-
-class _HedgehogCounterState extends State<HedgehogCounter> {
-  int _hedgehogCount = 0;
-  String _message = "";
-  final TextEditingController _controller = TextEditingController();
-
-  void _updateCounter(String input) {
-    String trimmedInput = input.trim();
-
-    if (trimmedInput == "я не знаю коду") {
-      setState(() {
-        _hedgehogCount = 0;
-        _message = "На перездачу!";
-      });
-    } else {
-      int? value = int.tryParse(trimmedInput);
-      if (value != null) {
-        setState(() {
-          _hedgehogCount += value;
-          _message = "";
-        });
-      }
-    }
-    _controller.clear();
-  }
-
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Лічильник їжаків")),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(90),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/logo.png', width: 300),
+              TextField(decoration: InputDecoration(hintText: 'Email', hintStyle: TextStyle(color: Color(0xFF292828)))),
+              TextField(obscureText: true, decoration: InputDecoration(hintText: 'Password', hintStyle: TextStyle(color: Color(0xFF292828)))),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => Navigator.pushNamed(context, '/home'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFFFFBD59),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+                ),
+                child: Text('Login', style: TextStyle(color: Color(0xFF292828))),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pushNamed(context, '/register'),
+                child: Text("Don't have an account? Sign up Here", style: TextStyle(color: Color(0xFF292828))),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RegisterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(90),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/logo.png', width: 300),
+              TextField(decoration: InputDecoration(hintText: 'Email', hintStyle: TextStyle(color: Color(0xFF292828)))),
+              TextField(decoration: InputDecoration(hintText: 'Phone Number', hintStyle: TextStyle(color: Color(0xFF292828)))),
+              TextField(obscureText: true, decoration: InputDecoration(hintText: 'Password', hintStyle: TextStyle(color: Color(0xFF292828)))),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => Navigator.pushNamed(context, '/home'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFFFFBD59),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+                ),
+                child: Text('Register', style: TextStyle(color: Color(0xFF292828))),
+              ),
+              TextButton(
+                  onPressed: () => Navigator.pushNamed(context, '/'),
+                  child: Text("Have an account? Sign in Here", style: TextStyle(color: Color(0xFF292828))),
+                  style:  TextButton.styleFrom()
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Profile'), backgroundColor: Color(0xFFF5E8CF)),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "${"🦔" * _hedgehogCount}",
-              style: TextStyle(fontSize: 30),
+            GestureDetector(
+              onTap: () => print('Change Avatar'),
+              child: CircleAvatar(radius: 50),
             ),
-            if (_message.isNotEmpty)
-              Padding(
-                padding: EdgeInsets.only(top: 10),
-                child: Text(
-                  _message,
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.red),
-                ),
-              ),
-            SizedBox(height: 20),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: TextField(
-                controller: _controller,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Введіть число або текст...",
-                ),
-                onSubmitted: _updateCounter,
-              ),
+            TextField(decoration: InputDecoration(hintText: 'User Name', hintStyle: TextStyle(color: Color(0xFF292828)))),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFFFFCF6),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Image.asset('assets/images/logo_home.png', width: 120),
+            GestureDetector(
+              onTap: () => Navigator.pushNamed(context, '/profile'),
+              child: CircleAvatar(radius: 20),
             ),
           ],
         ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildInfoCard('Avg Temp', '20°C'),
+                _buildInfoCard('Avg Humidity', '60%'),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 20,
+              itemBuilder: (context, index) => ListTile(
+                title: Text('Location $index', style: TextStyle(color: Color(0xFF292828))),
+                subtitle: Text('Temp: 20°C, Humidity: 60%', style: TextStyle(color: Color(0xFF292828))),
+                trailing: Text('Date: 26/03/2025', style: TextStyle(color: Color(0xFF292828))),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: ElevatedButton(
+              onPressed: () => print('Add Data'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFFFFBD59),
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+                textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              child: Text('Add New Data', style: TextStyle(color: Color(0xFF292828))),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildInfoCard(String title, String value) {
+    return Container(
+      width: 120,
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Color(0xFFFFBD59).withOpacity(0.6),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        children: [
+          Text(title, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Color(0xFF292828))),
+          SizedBox(height: 5),
+          Text(value, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xFF292828))),
+        ],
       ),
     );
   }
