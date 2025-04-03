63f1b95c8e5377b23c63a241e797809e6ead6e05
nataliahrabovska <natagrabovskanata@gmail.com>
2025-04-03 13:27:07 +0300

    fix: devided main.dart

---
+++ lib/screens/login_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(90),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/logo.png', width: 300),
+              _buildTextField('Email'),
+              _buildTextField('Password', obscureText: true),
+              SizedBox(height: 20),
+              _buildButton('Login', () => Navigator.pushNamed(context, '/home')),
+              TextButton(
+                onPressed: () => Navigator.pushNamed(context, '/register'),
+                child: Text(
+                  "Don't have an account? Sign up Here",
+                  style: TextStyle(color: Color(0xFF292828)),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget _buildTextField(String hint, {bool obscureText = false}) {
+  return TextField(
+    obscureText: obscureText,
+    decoration: InputDecoration(
+      hintText: hint,
+      hintStyle: TextStyle(color: Color(0xFF292828)),
+    ),
+  );
+}
+
+Widget _buildButton(String text, VoidCallback onPressed) {
+  return ElevatedButton(
+    onPressed: onPressed,
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Color(0xFFFFBD59),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(25),
+      ),
+    ),
+    child: Text(
+      text,
+      style: TextStyle(color: Color(0xFF292828)),
+    ),
+  );
+}
